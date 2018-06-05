d31efb8c4fdf4960e24d005f8db297505e76787d
LegalPrimes <david.palmer@outlook.com>
2018-06-05 15:35:06 -0400

    Create puzzle6.py

---
+++ puzzle6.py
@@ -0,0 +1,6 @@
+#read file and display on webpage
+var tid = setInterval(mycode, 2000);
+function mycode() {
+  // do some stuff...
+  // no need to recall the function (it's an interval, it$
+readTextFile("your file path");
