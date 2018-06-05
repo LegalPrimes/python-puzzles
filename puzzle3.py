e305a2e0a459782d3fcb0602553a3b9bd6c164f6
LegalPrimes <david.palmer@outlook.com>
2018-06-05 15:31:25 -0400

    Create puzzle3.py

---
+++ puzzle3.py
@@ -0,0 +1,6 @@
+#Puzzle #3
+yy = []
+for xx in range (0,100,5):
+        yy.append(xx)
+print ("the number of elements (a.k.a. length) in yy is %$for xx in range (19, -1, -1):
+        print (yy[xx])
