39c039713a5fc8f93e29b31d3a268c34f8c6e8df
LegalPrimes <david.palmer@outlook.com>
2018-06-05 15:38:13 -0400

    Create puzzle7.py

---
+++ puzzle7.py
@@ -0,0 +1,14 @@
+#Puzzle #4
+#Open a file.
+f=open('fortunes.txt', 'r') #'fortunes.txt' = the file path 'r' = read only parameter
+while (True):
+        #Read a line.
+        if line =="":
+                print ("******DONE******")
+                break
+        if line =="\n"
+                print ("***EMPTY LINE***")
+        #Print other lines.
+        stripped = line.strip()
+        print ("******LINE******")
+        print (stripped)
