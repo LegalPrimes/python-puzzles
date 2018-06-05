df8d62d4faa502628b8ddbfbe76773ec6a9e14a5
LegalPrimes <david.palmer@outlook.com>
2018-06-05 15:32:59 -0400

    Create puzzle4.py

---
+++ puzzle4.py
@@ -0,0 +1,8 @@
+#Puzzle #4
+import random
+yy = []
+for xx in range (20):
+        yy.append(random.randint (1,100))
+yy_sorted = sorted(yy) #set(yy))
+print ("the number of elements (a.k.a. length) in yy is %$for xx in range (-1,len(yy_sorted)):
+        print (yy_sorted[xx])
